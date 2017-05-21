1eae281f06c8b4b5347d3ba001c76458c1fa913d
Rogério Fontes <fontestz@gmail.com>
2017-05-21 10:05:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ths-blog
---
+++ thssitevenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rogeriofontes/Documents/Desenv/ths/ths-site/thssitevenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
